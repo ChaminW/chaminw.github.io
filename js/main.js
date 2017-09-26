633d70d6eb8379565772e9055b66f2658eb5c2a2
chaminw <chamin.13@cse.mrt.ac.lk>
2017-09-26 21:25:35 +0530

    Add typewriter effect

--- js/main.js
+++ js/main.js
@@ -7,19 +7,48 @@ $(window).load(function(){
 
 
 	//==============___Page Loader___================
-  
-  $('#page-loader').delay(300).fadeOut(400, function(){
 
+
+
+    $('#page-loader').delay(300).fadeOut(400, function(){
   });
 
   $('#loader-name').addClass('loader-left');
   $('#loader-job').addClass('loader-right');
   $('#loader-animation').addClass('loader-hide');
 
+
+
 });
 
 //Begin - Document Ready
 $(document).ready(function(){
+
+    //==============___TypeWriter___================
+    var app = document.getElementById('body-job');
+
+    var typewriter = new Typewriter(app, {
+        loop: true
+    });
+    typewriter.typeString('Software Engineer')
+        .pauseFor(2500)
+        .deleteAll()
+        .typeString('Web developer')
+        .pauseFor(2500)
+        .deleteAll()
+        .typeString('Programmer')
+        .pauseFor(2500)
+        .deleteAll()
+        .typeString('Tech Geek')
+        .pauseFor(2500)
+        .deleteAll()
+        .typeString('Blogger')
+        .pauseFor(2500)
+        .deleteAll()
+        .typeString('Graphic Designer')
+        .pauseFor(2500)
+        .start();
+
 
 //==============___Page Loader___================
   $('#loading-wraper').fadeIn(300);

2dd458881e6905aac0a1fa524e9687c2a634edcb
chaminw <chamin.13@cse.mrt.ac.lk>
2017-09-30 12:17:51 +0530

    Change particle js directory

---
+++ js/particle-script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by chamin on 9/30/17.
+ */
+
+$(document).ready(function() {
+
+    //==============___Particle js___================
+    particlesJS.load('particles-js', 'assets/particlesjs-config.json', function () {
+        console.log('callback - particles.js config loaded');
+    });
+
+});
